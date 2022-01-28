7ed697bbcf6a29633a9ff019a5e6a17dedf2492f
Chad Hatcher <chatcher@users.noreply.github.com>
2022-01-27 20:15:08 -0800

    hooray component autoloader thingy

--- src/engine/loader.js
+++ src/engine/loader.js
@@ -48,17 +48,76 @@ const stupidTemplateComponent = (route, controllerModule) => {
 	};
 };
 
+const setupStupidComponentAutoloader = (context) => {
+	class StupidComponentAutoloader extends HTMLElement {
+		context = context;
+		module = null;
+
+		get controllerClassName() {
+			const {
+				name: componentName,
+			} = this.context;
+
+			return `${componentName.replace(
+				/(?:\b|\W)(\w)/g,
+				(_, letter) => letter.toUpperCase()
+			)}Controller`;
+		}
+
+		get Controller() {
+			return this.module[this.controllerClassName];
+		}
+
+		constructor() {
+			super();
+			console.log('StupidComponentAutoloader::constructor()', { context });
+			this.innerHTML = `<p>Loading ${context.name}...</p>`;
+			this.awesome();
+		}
+
+		async awesome() {
+			if (context.template) {
+				const templateResponse = await fetch(context.template);
+				if (templateResponse.ok) {
+					this.innerHTML = await templateResponse.text();
+				} else {
+					this.innerHTML = `<pre>Error: ${
+						JSON.stringify(templateResponse, null, 2)
+							.replace(/[<>&"']/g, ch => ({
+								'<' : '&lt;',
+								'>' : '&gt;',
+								'&' : '&amp;',
+								'"' : '&quot;',
+								"'" : '&apos;',
+							}[ch]))}</pre>`;
+					console.error('fetch template', { context, templateResponse });
+				}
+			}
+
+			if (context.controller) {
+				this.module = await import(context.controller);
+				this.controller = new this.Controller(this);
+			}
+		}
+	}
+
+	customElements.define(
+		context.name,
+		StupidComponentAutoloader,
+	);
+};
+
 class Loader {
 	promises = {};
 
 	constructor(components) {
 		Object.values(components).forEach((component) => {
-			this.loadComponentTemplate(component);
+			setupStupidComponentAutoloader(component);
 		});
 	}
 
-	async loadTemplate(context) {
-		console.log('Loader::loadTemplate()', { context });
+	loadRouteTemplate(context) {
+		console.log('Loader::loadRouteTemplate()', { context });
 
 		if (!this.promises[context.name]) {
 			this.promises[context.name] = new Promise(async (resolve, reject) => {
@@ -88,14 +147,6 @@ class Loader {
 		}
 
 		return this.promises[context.name];
-	}
-
-	loadComponentTemplate(component) {
-		return this.loadTemplate(component);
-	}
-
-	loadRouteTemplate(route) {
-		return this.loadTemplate(route);
 	}
 }
 
