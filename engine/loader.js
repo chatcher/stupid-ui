fa7daa8c600175d70fac0059f1e41401850ddac1
Chad Hatcher <chatcher@users.noreply.github.com>
2022-01-28 22:15:31 -0800

    rename file

--- engine/loader.js
+++ engine/loader.js
@@ -1,6 +1,6 @@
 import { components } from '../components.js';
 import { setupStupidComponentAutoloader } from './components.js'
-import { router } from './routing.js';
+import { router } from './router.js';
 
 Object.values(components).forEach((component) => {
 	setupStupidComponentAutoloader(component, router);
