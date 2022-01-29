76e7cf0ac67acee745a7adcf5d22eadcfffe6cfd
Chad Hatcher <chatcher@users.noreply.github.com>
2022-01-28 21:37:37 -0800

    pull lots of stuff out of the class and into the loader scope

--- engine/components.js
+++ engine/components.js
@@ -13,55 +13,39 @@
 // }
 
 export const setupStupidComponentAutoloader = async (context) => {
+	let template = `<p>No template for ${context.name}</p>`;
+
+	if (context.template) {
+		const templateResponse = await fetch(context.template);
+		if (templateResponse.ok) {
+			template = await templateResponse.text();
+		} else {
+			template = `<pre>Error: ${JSON.stringify(templateResponse, null, 2)
+				.replace(/[<>&"']/g, (ch) => ({
+					'<': '&lt;',
+					'>': '&gt;',
+					'&': '&amp;',
+					'"': '&quot;',
+					'\'': '&apos;',
+				}[ch]))
+			}</pre>`;
+			console.error('fetch template', { context, templateResponse });
+		}
+	}
+
+	const controllerModule = context.controller ? await import(context.controller) : null;
+	const controllerClassName = `${context.name.replace(
+		/(?:\b|\W)(\w)/g,
+		(_, letter) => letter.toUpperCase()
+	)}Controller`;
+	const Controller = controllerModule ? controllerModule[controllerClassName] : null;
+
 	class StupidComponentAutoloader extends HTMLElement {
-		context = context;
-		module = null;
-
-		get controllerClassName() {
-			const {
-				name: componentName,
-			} = this.context;
-
-			return `${componentName.replace(
-				/(?:\b|\W)(\w)/g,
-				(_, letter) => letter.toUpperCase()
-			)}Controller`;
-		}
-
-		get Controller() {
-			return this.module[this.controllerClassName];
-		}
-
 		constructor() {
 			super();
 			console.log('StupidComponentAutoloader::constructor()', { context });
-			this.innerHTML = `<p>Loading ${context.name}...</p>`;
-			this.awesome();
-		}
-
-		async awesome() {
-			if (context.template) {
-				const templateResponse = await fetch(context.template);
-				if (templateResponse.ok) {
-					this.innerHTML = await templateResponse.text();
-				} else {
-					this.innerHTML = `<pre>Error: ${JSON.stringify(templateResponse, null, 2)
-						.replace(/[<>&"']/g, (ch) => ({
-							'<': '&lt;',
-							'>': '&gt;',
-							'&': '&amp;',
-							'"': '&quot;',
-							'\'': '&apos;',
-						}[ch]))
-					}</pre>`;
-					console.error('fetch template', { context, templateResponse });
-				}
-			}
-
-			if (context.controller) {
-				this.module = await import(context.controller);
-				this.controller = new this.Controller(this);
-			}
+			this.innerHTML = template;
+			this.controller = Controller ? new Controller(this) : null;
 		}
 	}
 
