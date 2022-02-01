2e32712e7f8b67a558784cc19befec3ba9d00cf4
Chad Hatcher <chatcher@users.noreply.github.com>
2022-01-31 20:58:24 -0800

    add a fancy-value component

---
+++ example/client/components/fancy-value/fancy-value.js
@@ -0,0 +1,17 @@
+class StupidBaseComponent {
+	onInit() {}
+
+	constructor(element) {
+		this.$element = element;
+	}
+
+	$emit(name, payload) {
+		const event = new Event(name, { bubbles: true });
+		event.payload = payload;
+		this.$element.dispatchEvent(event);
+	}
+}
+
+export class FancyValueController extends StupidBaseComponent {
+	value = 'none';
+}
--- example/client/components/some-component/some-component.js
+++ example/client/components/some-component/some-component.js
@@ -1,5 +1,32 @@
-export class SomeComponentController {
-	constructor() {
-		console.log('Some Component');
+class StupidBaseComponent {
+	onInit() {}
+
+	constructor(element) {
+		this.$element = element;
+
+		// const props = Object.keys(this);
+		// console.log('StupidBaseComponent', { props });
+		// Object.keys(this).forEach((key) => {
+		// 	console.log(key);
+		// });
+	}
+
+	$emit(name, payload) {
+		const event = new Event(name, { bubbles: true });
+		event.payload = payload;
+		this.$element.dispatchEvent(event);
+	}
+}
+
+export class SomeComponentController extends StupidBaseComponent {
+	someDynamic = 'some component dynamic prop';
+
+	constructor(element) {
+		super(element);
+	}
+
+	onInit() {
+		this.$emit('some-event', { success: true });
+		// this.element.dispatchEvent(new Event('some-event', { bubbles: true }));
 	}
 }
