02f2fa10768b673efe0553a44364f0e78acb6144
Chad Hatcher <chatcher@users.noreply.github.com>
2022-02-05 07:18:43 -0800

    add an iterative example component

---
+++ example/client/components/iterative-component/iterative-component.js
@@ -0,0 +1,9 @@
+import { StupidBaseComponent } from '/engine/components/stupid-base-component.js';
+
+export class IterativeComponentController extends StupidBaseComponent {
+	list = [
+		'one',
+		'two',
+		'three',
+	];
+}
