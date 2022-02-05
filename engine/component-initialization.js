041ad85523618e5b832ea504323daf5975a0e18a
Chad Hatcher <chatcher@users.noreply.github.com>
2022-02-05 11:32:50 -0800

    re-order methods by usage

--- engine/component-initialization.js
+++ engine/component-initialization.js
@@ -1,16 +1,3 @@
-
-// connectedCallback() {
-// 	if (!closure.initialized) setTimeout(() => {
-// 		this.connectHeirarchy();
-// 		this.initializeTemplate(this.controller.$template || template);
-// 		if (this.controller && this.controller.onInit) {
-// 			this.controller.onInit();
-// 		}
-// 	});
-
-// 	closure.initialized = true;
-// }
-
 export function connectHeirarchy(element) {
 	notifyParentComponent(element);
 	listenForChildComponents(element);
@@ -32,91 +19,27 @@ export function initializeTemplate(element, template) {
 
 ////////////////////////////////////////////////////////////
 
-function bindTemplateSlots(element, template) {
-	const bindings = template.trim().split('{{');
-	const templateParts = [bindings.shift()];
-	bindings.map((fragment) => fragment.split('}}'))
-		.forEach(([unsafeExpression, trailer]) => {
-			const slot = getBindingReplacementSlot(element, unsafeExpression);
-			templateParts.push(slot);
-			templateParts.push(trailer);
-		});
-
-	element.innerHTML = templateParts.join('');
+function notifyParentComponent(element) {
+	element.dispatchEvent(new Event('stupid-component-created', { bubbles: true }));
 }
 
-function getBindingReplacementSlot(element, unsafeExpression) {
-	const expression = unsafeExpression.trim();
-
-	if (expression in element.controller) {
-		const slotName = `${element.componentId}-slot-${Math.random().toString(16).substr(2)}`;
-		return `<slot
-			name="${slotName}"
-			/><span
-			data-component="${element.componentId}"
-			data-expression="${expression}"
-			slot="${slotName}"
-			>...</span>`;
-	}
-
-	return `{{${expression}}}`;
-}
-
-function initializeTemplateIterations(element) {
-	const iterations = Array.from(element.querySelectorAll('[for-each]'));
-
-	if (iterations.length) {
-		console.group()
-		console.log(element, { element });
-		iterations.forEach((iterator, index) => {
-			const itemName = iterator.getAttribute('for-each');
-			const listName = iterator.getAttribute('#in');
-
-			console.group(`found #${index + 1}`);
-			console.log(iterator, { iterator });
-			console.log({ 'for each': itemName });
-			console.log({ in: listName });
-
-			if (listName in element.controller) {
-				const list = Array.from(element.controller[listName]);
-				console.log('list:', list);
-			} else {
-				console.error('Property not found for iteration:', listName);
-			}
-
-			console.groupEnd();
-		});
-		console.groupEnd();
-	}
-}
-
-function populateTemplate(element) {
-	const bindings = element.querySelectorAll(`span[data-component="${element.componentId}"][data-expression]`);
-	Array.from(bindings).forEach((span) => {
-		const expression = span.getAttribute('data-expression');
-		span.innerText = executeTemplateExpression(element, expression.trim());
+function listenForChildComponents(element) {
+	element.addEventListener('stupid-component-created', (event) => {
+		event.stopPropagation();
+		const child = event.target;
+		console.debug(`StupidComponent<${element.context.name}>::child-attached`, child, { child });
+		element.children.push(child);
+		watchChildEvents(element, child);
+		bindDataToChild(element, child);
 	});
-}
 
-function executeTemplateExpression(element, expression) {
-	if (expression in element.controller) {
-		return element.controller[expression];
-	}
-
-	console.group(element.context.name);
-	console.error('Unknown/unsupported template expression:', expression);
-	console.log(element);
-	console.log({ element });
-	console.log(element, element.controller);
-	console.log({ controler: element.controller });
-	console.log(element, element.controller[expression]);
-	console.log({
-		expression,
-		value: element.controller[expression],
+	element.addEventListener('stupid-component-removed', () => {
+		event.stopPropagation();
+		const child = event.target;
+		console.warn(`StupidComponent<${element.context.name}>::child-detached`, child, { child });
+		const index = element.children.indexOf(child);
+		element.children.splice(index, 1);
 	});
-	console.groupEnd();
-
-	return `{{${expression}}}`;
 }
 
 function convertPropertiesToWatchedProperties(element) {
@@ -141,28 +64,68 @@ function convertPropertiesToWatchedProperties(element) {
 		});
 }
 
-function notifyParentComponent(element) {
-	element.dispatchEvent(new Event('stupid-component-created', { bubbles: true }));
+function bindTemplateSlots(element, template) {
+	const bindings = template.trim().split('{{');
+	const templateParts = [bindings.shift()];
+	bindings.map((fragment) => fragment.split('}}'))
+		.forEach(([unsafeExpression, trailer]) => {
+			const slot = getBindingReplacementSlot(element, unsafeExpression);
+			templateParts.push(slot);
+			templateParts.push(trailer);
+		});
+
+	element.innerHTML = templateParts.join('');
 }
 
-function listenForChildComponents(element) {
-	element.addEventListener('stupid-component-created', (event) => {
-		event.stopPropagation();
-		const child = event.target;
-		console.debug(`StupidComponent<${element.context.name}>::child-attached`, child, { child });
-		element.children.push(child);
-		watchChildEvents(element, child);
-		bindDataToChild(element, child);
-	});
+function initializeTemplateIterations(element) {
+	const iterations = Array.from(element.querySelectorAll('[for-each]'));
 
-	element.addEventListener('stupid-component-removed', () => {
-		event.stopPropagation();
-		const child = event.target;
-		console.warn(`StupidComponent<${element.context.name}>::child-detached`, child, { child });
-		const index = element.children.indexOf(child);
-		element.children.splice(index, 1);
+	if (iterations.length) {
+		console.group()
+		console.log(element, { element });
+		iterations.forEach((iteration, index) => {
+			const itemName = iteration.getAttribute('for-each');
+			const listName = iteration.getAttribute('#in');
+
+			console.group(`found #${index + 1}`);
+			console.log(iteration, { iteration });
+			console.log({ 'for each': itemName });
+			console.log({ in: listName });
+
+			if (listName in element.controller) {
+				const list = Array.from(element.controller[listName]);
+				console.log('list:', list);
+
+				list.forEach((item) => {
+					const injection = iteration.cloneNode(true);
+					injection.removeAttribute('for-each');
+					injection.removeAttribute('#in');
+
+					console.log([injection]);
+					iteration.insertAdjacentElement('afterend', injection);
+				});
+
+				iteration.remove();
+
+			} else {
+				console.error('Property not found for iteration:', listName);
+			}
+
+			console.groupEnd();
+		});
+		console.groupEnd();
+	}
+}
+
+function populateTemplate(element) {
+	const bindings = element.querySelectorAll(`span[data-component="${element.componentId}"][data-expression]`);
+	Array.from(bindings).forEach((span) => {
+		const expression = span.getAttribute('data-expression');
+		span.innerText = executeTemplateExpression(element, expression.trim());
 	});
 }
+
+////////////////////////////////////////////////////////////
 
 function watchChildEvents(element, child) {
 	Array.from(child.attributes)
@@ -208,4 +171,42 @@ function bindDataToChild(element, child) {
 				console.error(error);
 			}
 		});
+}
+
+function getBindingReplacementSlot(element, unsafeExpression) {
+	const expression = unsafeExpression.trim();
+
+	if (expression in element.controller) {
+		const slotName = `${element.componentId}-slot-${Math.random().toString(16).substr(2)}`;
+		return `<slot
+			name="${slotName}"
+			/><span
+			data-component="${element.componentId}"
+			data-expression="${expression}"
+			slot="${slotName}"
+			>...</span>`;
+	}
+
+	return `{{${expression}}}`;
+}
+
+function executeTemplateExpression(element, expression) {
+	if (expression in element.controller) {
+		return element.controller[expression];
+	}
+
+	console.group(element.context.name);
+	console.error('Unknown/unsupported template expression:', expression);
+	console.log(element);
+	console.log({ element });
+	console.log(element, element.controller);
+	console.log({ controler: element.controller });
+	console.log(element, element.controller[expression]);
+	console.log({
+		expression,
+		value: element.controller[expression],
+	});
+	console.groupEnd();
+
+	return `{{${expression}}}`;
 }
