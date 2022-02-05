c869433a2713b1d381e0c8581e5b6bf79a11a430
Chad Hatcher <chatcher@users.noreply.github.com>
2022-02-05 07:26:25 -0800

    remove commented lines

--- engine/components.js
+++ engine/components.js
@@ -2,19 +2,15 @@ export const components = {
   "stupid-switch": {
     "name": "stupid-switch",
     "files": [
-      // "/components/stupid-switch/stupid-switch.html",
       "/engine/components/stupid-switch/stupid-switch.js"
     ],
-    // "template": "/components/stupid-switch/stupid-switch.html",
     "controller": "/engine/components/stupid-switch/stupid-switch.js"
   },
   "switch-case": {
     "name": "switch-case",
     "files": [
-      // "/components/switch-case/switch-case.html",
       "/engine/components/switch-case/switch-case.js"
     ],
-    // "template": "/components/switch-case/switch-case.html",
     "controller": "/engine/components/switch-case/switch-case.js"
   },
 };
