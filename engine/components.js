9f0cd6243b99e042aefb2a0cafc9b25ead6517e2
Chad Hatcher <chatcher@users.noreply.github.com>
2022-02-06 12:51:19 -0800

    fix indentation

--- engine/components.js
+++ engine/components.js
@@ -1,16 +1,16 @@
 export const components = {
-  "stupid-switch": {
-    "name": "stupid-switch",
-    "files": [
-      "/engine/components/stupid-switch/stupid-switch.js"
-    ],
-    "controller": "/engine/components/stupid-switch/stupid-switch.js"
-  },
-  "switch-case": {
-    "name": "switch-case",
-    "files": [
-      "/engine/components/switch-case/switch-case.js"
-    ],
-    "controller": "/engine/components/switch-case/switch-case.js"
-  },
+	'stupid-switch': {
+		name: 'stupid-switch',
+		files: [
+			'/engine/components/stupid-switch/stupid-switch.js',
+		],
+		controller: '/engine/components/stupid-switch/stupid-switch.js',
+	},
+	'switch-case': {
+		name: 'switch-case',
+		files: [
+			'/engine/components/switch-case/switch-case.js',
+		],
+		controller: '/engine/components/switch-case/switch-case.js',
+	},
 };
